734b9783e3e52f0a38e7723d9cc73370e7c18b1c
Roman Turner <rn.turner@live.com>
2021-01-19 20:00:06 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,16 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :partiarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => [{},{}] 
+     },
+     :capulet => {
+       :partiarch => {},
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => [{},{}] 
+     }
+   }
 end
